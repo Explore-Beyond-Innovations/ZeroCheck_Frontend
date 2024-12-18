5527365b1cfb28122fdb4d1274c809330ebd4a1c
holamite <olamideadebara@gmail.com>
2024-12-18 20:52:00 +0100

    Created files

---
+++ app/check-in/page.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import CheckInModal from "../components/Check-In";
+
+const page = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  return <div></div>;
+};
+
+export default page;
